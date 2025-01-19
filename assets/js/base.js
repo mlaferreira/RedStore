f00c36fde288b438b0c42e57799ef94003b64722
mlaferreira <mlaferreira.marcello@gmail.com>
2025-01-19 19:40:51 -0300

    escrevendo o js do menu

--- assets/js/base.js
+++ assets/js/base.js
@@ -2,10 +2,11 @@ let iconMenu = document.querySelector('.menu-icon');
 let menuItems = document.querySelector('#menuItems');
 
 menuItems.style.maxHeight = '0px';
+
 iconMenu.addEventListener('click', menutoggle);
 
 function menutoggle(){
-    if(menuItems.style.maxHeight == '0px'){
+    if(menuItems.style.maxHeight === '0px'){
         menuItems.style.maxHeight = '200px';
     }else{
         menuItems.style.maxHeight = '0px';
